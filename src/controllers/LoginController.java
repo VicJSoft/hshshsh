b011136e4e6f139e007a3397533da69ae324be84
VicenteAguileraPerez <vicente_prea@hotmail.com>
2019-08-28 14:59:37 -0500

    [ADD] information about the project, this information was created by Victor Espino

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,109 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private JFXCheckBox cb_recordar;
+    @FXML
+    private Button btn_login;
+    @FXML
+    private Button btn_cerrar;
+    
+    @FXML
+    private AnchorPane fondoAP;
+    private double xOffset = 0;
+    private double yOffset = 0;
+    @FXML
+    private JFXTextField txt_usuario;
+    @FXML
+    private JFXPasswordField txt_contrasena;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        setValidatorsRequired();
+
+
+        
+    }
+
+    private void setValidatorsRequired(){
+        
+      
+        txt_usuario.getValidators().add(new RequiredFieldValidator("Este campo no debe estar vacío..."));
+        
+        txt_usuario.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if(!newValue)
+                  txt_usuario.validate();
+            }
+        });
+        
+         
+         txt_contrasena.getValidators().add(new RequiredFieldValidator("Este campo no debe estar vacío..."));
+        
+        txt_contrasena.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if(!newValue)
+                    txt_contrasena.validate();
+            }
+        });
+        
+    }
+    
+
+    @FXML
+    private void dragWindow_Click(MouseEvent event) {
+
+        Stage primaryStage = (Stage)( ((Node) (event.getSource() ) ).getScene().getWindow() );
+        primaryStage.setX(event.getScreenX()- xOffset);
+        primaryStage.setY(event.getScreenY() - yOffset );
+    }
+
+    @FXML
+    private void pressedWindow_Click(MouseEvent event) {
+        xOffset = event.getSceneX();//guarda coord iniciales del clic
+        yOffset = event.getSceneY();
+    }
+
+
+    @FXML
+    private void cbRecordar_Click(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void btnLogin_Click(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void btnCerrar_Click(ActionEvent event) {
+        ((Stage)((Node)event.getSource()).getScene().getWindow()).close();
+    }
+    
+    
+}
